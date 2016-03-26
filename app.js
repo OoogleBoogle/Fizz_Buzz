dfd1edc37783c0109b1e7d815d18ff19a4f7b06c
OoogleBoogle <simattfield@gmail.com>
2016-03-26 14:45:04 -0700

    FIN

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
   var list = document.getElementById('fizz-buzz-list');
 
-  i = 0; 
-  while (i <= 100) {
-    html = "<li>";
-    if (i % 3 === 0) && (i % 5 === 0) {
-      html += "FizzBuzz</li>";
-      list.
+  // i = 1;
+  // while (i <= 100) {
+  //   if (i % 3 === 0 && i % 5 === 0) {
+  //     list.innerHTML += "<li>FizzBuzz</li>";
+  //   } else if (i % 3 === 0) {
+  //     list.innerHTML += "<li>Fizz</li>";
+  //   } else if (i % 5 === 0) {
+  //     list.innerHTML += "<li>Buzz</li>";
+  //   } else {
+  //     list.innerHTML += "<li>" + i + "</li>";
+  //   }
+  //   i++
+  // }
+
+  for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      list.innerHTML += '<li class="red">Fizz Buzz</li>';
     } else if (i % 3 === 0) {
-
+      list.innerHTML += '<li class="green">Fizz</li>';
     } else if (i % 5 === 0) {
-
+      list.innerHTML += '<li class="blue">Buzz</li>';
     } else {
-
+      list.innerHTML += "<li>" + i + "</li>";
     }
   }
 });
-
-
