35b8cf44c64964d2175ad15238d4829a5a2e9c55
OoogleBoogle <simattfield@gmail.com>
2016-03-26 13:27:03 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var list = document.getElementById('fizz-buzz-list');
+
+  i = 0; 
+  while (i <= 100) {
+    html = "<li>";
+    if (i % 3 === 0) && (i % 5 === 0) {
+      html += "FizzBuzz</li>";
+      list.
+    } else if (i % 3 === 0) {
+
+    } else if (i % 5 === 0) {
+
+    } else {
+
+    }
+  }
+});
+
+
